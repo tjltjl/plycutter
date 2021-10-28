1c0a72b0dae0049d30db19cb71350c932fbcb880
Mario Voigt <mario.voigt@stadtfabrikanten.org>
2021-10-28 02:24:47 +0200

    added basic svg export function

--- plycutter/writer.py
+++ plycutter/writer.py
@@ -26,6 +26,49 @@ from .geometry.aabb import AABB
 logger = logging.getLogger(__name__)
 
 
+def write_svg(filename, geom2ds):
+    file = open(filename, 'w')
+    file.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n")
+    file.write("<svg version = \"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n")
+    
+    # Trivial nesting horizontally
+    x = 0
+    for name, geom in geom2ds.items():
+        print(name)
+        if geom.is_empty():
+            logger.warn(f'Empty sheet {name}')
+            continue
+
+        aabb = AABB()
+
+        for polygon in geom.polygons():
+            coords = np.array(polygon.spwhs[0].outer)
+            for pt in coords:
+                aabb.include_point(pt)
+
+        margin = 3
+        x_offset = x - aabb.lower[0] + margin
+        y_offset = 0 - aabb.lower[1]
+        x += aabb.upper[0] - aabb.lower[0] + margin
+
+        def draw_coords(coords):
+            coords = list(coords)
+            coords = np.array(coords + coords[0:1])  # Loop
+            coords = coords + [x_offset, y_offset]
+            coords = coords.astype(np.float64)
+            assert np.all(np.isfinite(coords))
+            path = "M "
+            for coord in coords:
+                path += "{:0.6f},{:0.6f} ".format(coord[0], coord[1])
+            file.write("<path d =\"" + path + "\" style=\"fill:none;stroke:#000000;stroke-width:1px;stroke-opacity:1.0\" />\n")
+        for polygon in geom.polygons():
+            draw_coords(polygon.spwhs[0].outer)
+            for hole in polygon.spwhs[0].holes:
+                draw_coords(hole)
+    file.write("</svg>")
+    file.close()
+
+
 def write_dxf(filename, geom2ds):
     dwg = ezdxf.new('AC1015')
     modelspace = dwg.modelspace()
